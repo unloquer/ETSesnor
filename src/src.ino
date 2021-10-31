71df09bce542bb3608fc9def3211f01d6aa3c816
jero98772 <jero98772@protonmail.com>
2021-10-31 14:29:22 -0500

    add leds code

--- src/src.ino
+++ src/src.ino
@@ -1,6 +1,38 @@
-//archivo base
-void setup() {
+#include <FastLED.h>
+#include <TaskScheduler.h>
+
+#define NUM_LEDS 64
+#define LED_PIN D6
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+int BRIGHTNESS = 10; 
+bool ledToggle = false;
+CRGB leds[NUM_LEDS];
+
+CRGB setColor(){
+  CRGB color = CRGB::Gold;
+  return color ;
+
+}
+void setLed(){
+  ledToggle = !ledToggle;
+    for(int i=0; i < NUM_LEDS; i++) leds[i] = ledToggle ? setColor() : CRGB::Black;
+      FastLED.show();
 }
 
-void loop() {
+Scheduler runner;
+Task ledBlink(1000, TASK_FOREVER, &setLed);
+
+void setup() {
+    runner.init();
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println("1");
+    FastLED.setBrightness(BRIGHTNESS);
+    LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    runner.addTask(ledBlink);
+    ledBlink.enable();
+}
+void loop() {  
+    runner.execute();
 }
