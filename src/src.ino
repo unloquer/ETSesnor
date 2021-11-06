06a0b4f785a2398128520bb4ed1b933517e44895
jero98772 <jero98772@protonmail.com>
2021-11-06 15:48:26 -0500

    update mic ,add jhonny code

--- src/src.ino
+++ src/src.ino
@@ -1,13 +1,42 @@
 #include <FastLED.h>
 #include <TaskScheduler.h>
-
-#define NUM_LEDS 64
+const uint8_t kMatrixWidth  = 8;
+const uint8_t kMatrixHeight = 8;
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
 #define LED_PIN D6
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
+#define analogpin = 0;//mic pin
 int BRIGHTNESS = 10; 
 bool ledToggle = false;
 CRGB leds[NUM_LEDS];
+
+ 
+int loop_cnt = 0;
+ 
+const int sampleWindow = 50;
+unsigned int sample;
+ 
+uint8_t noise[kMatrixWidth][kMatrixHeight];
+
+#define ESCENAS 1 
+#define amarillo CRGB::Yellow 
+#define black CRGB::Black 
+#define rojo CRGB::Red
+#define blue CRGB::Blue
+ 
+CRGB matrix[ESCENAS][8][8] = {
+  {
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {blue, blue, blue,blue, blue, blue, blue,blue},
+  },
+};
 
 CRGB setColor(){
   CRGB color = CRGB::Gold;
@@ -35,4 +64,32 @@ void setup() {
 }
 void loop() {  
     runner.execute();
+ 
+  unsigned long startMillis= millis();  // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+ 
+ while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(analogpin);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+ 
+  int cambiarBrillo = map(volts, 0, 5, 0, 100);
+
+    loop_cnt++;
 }
