1216492f56b710c1295690089a81adac8253695f
Johnny sepúlveda <johnnysepulveda988@gmail.com>
2021-11-11 23:37:34 -0500

    Update src.ino
    cambio nombre de variables, elimino código que no se usa, dejo comentarios necesarios

--- src/src.ino
+++ src/src.ino
@@ -1,95 +1,75 @@
 #include <FastLED.h>
-#include <TaskScheduler.h>
-const uint8_t kMatrixWidth  = 8;
-const uint8_t kMatrixHeight = 8;
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-#define LED_PIN D6
+#define LED_PIN D2
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-#define analogpin = 0;//mic pin
-int BRIGHTNESS = 10; 
-bool ledToggle = false;
-CRGB leds[NUM_LEDS];
+#define amarillo CRGB::Yellow 
+#define negro CRGB::Black 
+#define rojo CRGB::Red
+#define azul CRGB::Blue
 
+const uint8_t matrixWidth  = 8;
+const uint8_t matrixHeight = 8;
+#define NUM_LEDS (matrixWidth * matrixHeight)
+
+int BRIGHTNESS = 60;
+CRGB leds[matrixWidth * matrixHeight];
  
 int loop_cnt = 0;
- 
-const int sampleWindow = 50;
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
  
-uint8_t noise[kMatrixWidth][kMatrixHeight];
-
-#define ESCENAS 1 
-#define amarillo CRGB::Yellow 
-#define black CRGB::Black 
-#define rojo CRGB::Red
-#define blue CRGB::Blue
+void setup() {
+  Serial.begin(115200);
+  LEDS.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds,NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+}
  
+#define ESCENAS 1
 CRGB matrix[ESCENAS][8][8] = {
   {
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
-    {blue, blue, blue,blue, blue, blue, blue,blue},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
+    {azul, azul, azul,azul, azul, azul, azul,azul},
   },
 };
-
-CRGB setColor(){
-  CRGB color = CRGB::Gold;
-  return color ;
-
-}
-void setLed(){
-  ledToggle = !ledToggle;
-    for(int i=0; i < NUM_LEDS; i++) leds[i] = ledToggle ? setColor() : CRGB::Black;
-      FastLED.show();
-}
-
-Scheduler runner;
-Task ledBlink(1000, TASK_FOREVER, &setLed);
-
-void setup() {
-    runner.init();
-    Serial.begin(115200);
-    Serial.setDebugOutput(true);
-    Serial.println("1");
-    FastLED.setBrightness(BRIGHTNESS);
-    LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-    runner.addTask(ledBlink);
-    ledBlink.enable();
-}
-void loop() {  
-    runner.execute();
  
-  unsigned long startMillis= millis();  // Start of sample window
-  unsigned int peakToPeak = 0;   // peak-to-peak level
+void loop() {
+  for(int i = 0; i< matrixHeight; i++) {
+    for(int j = 0; j< matrixWidth; j++) {
+      leds[i*matrixWidth + j] = matrix[loop_cnt%ESCENAS][i][j];
+    }
+  }
+  unsigned long startMillis = millis();  // Start of sample window
+  unsigned int peakToPeak = 0;
  
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
  
- while (millis() - startMillis < sampleWindow)
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow)
    {
-      sample = analogRead(analogpin);
-      if (sample < 1024)  // toss out spurious readings
-      {
-         if (sample > signalMax)
-         {
-            signalMax = sample;  // save just the max levels
-         }
-         else if (sample < signalMin)
-         {
-            signalMin = sample;  // save just the min levels
-         }
+      sample = analogRead(0);
+      if (sample < 1024) {
+        if (sample > signalMax)
+        {
+          signalMax = sample;
+        }
+        else if (sample < signalMin)
+        {
+          signalMin = sample;
+        }
       }
    }
-   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
- 
-  int cambiarBrillo = map(volts, 0, 5, 0, 100);
-
-    loop_cnt++;
+   peakToPeak = signalMax - signalMin;
+   int changeBrightness = map(peakToPeak, 20, 500, 0, 100);
+   FastLED.setBrightness(changeBrightness);
+   FastLED.show();
+   Serial.println(peakToPeak);
+   loop_cnt++;
+   FastLED.clear();
 }
