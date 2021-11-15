aac6c5848630291dd0ecc5ae30cb03b05b94e585
jero98772 <jero98772@protonmail.com>
2021-11-15 10:50:21 -0500

    add 2 states chnage

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,5 @@
 #include <FastLED.h>
+
 #define LED_PIN D3
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
@@ -7,22 +8,21 @@
 #define rojo CRGB::Red
 #define azul CRGB::Blue
 
-
+int loop_cnt = 0;
+const uint8_t matrixWidth =8;
+const uint8_t matrixHeight=8;
+#define NUM_LEDS (matrixWidth * matrixHeight)
 #define ESCENAS 1
 
 
-const uint8_t matrixSize  = 8;
-#define NUM_LEDS (matrixSize * matrixSize)
-
 int BRIGHTNESS = 60;
-CRGB leds[matrixSize* matrixSize];
- 
-int loop_cnt = 0;
+CRGB leds[matrixWidth * matrixHeight]; 
+
 const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
-
-CRGB matrixBajo[ESCENAS][matrixSize][matrixSize] = {
-  {
+int ruidoMedio=800/2;
+//dibujo 1
+CRGB matrixBajo[matrixHeight][matrixWidth] = {
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
@@ -31,10 +31,9 @@ CRGB matrixBajo[ESCENAS][matrixSize][matrixSize] = {
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
-  },
 };
-CRGB matrixAlto[ESCENAS][matrixSize][matrixSize] = {
-  {
+//dibujo 2
+CRGB matrixAlto [matrixHeight][matrixWidth] = {
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
@@ -43,23 +42,23 @@ CRGB matrixAlto[ESCENAS][matrixSize][matrixSize] = {
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
     {azul, azul, azul,azul, azul, azul, azul,azul},
-  },
 };
 
 
-
-void stateHighLow(bool operation,CRGB m1,CRGB m2){
+/*funcion para solo 2 estados*/
+void stateHighLow(bool operation,CRGB m1[matrixHeight][matrixWidth],CRGB m2[matrixHeight][matrixWidth]){
   if(operation){
-    
+    changeLed(m1);
+  }else{
+    changeLed(m2);
   }
 }
-void changeLed(){
-  for(int i = 0; i< matrixSize; i++) {
-    for(int j = 0; j< matrixSize; j++) {
-      leds[i*matrixSize + j] = matrix[loop_cnt%ESCENAS][i][j];
+void changeLed(CRGB matrix[matrixHeight][matrixWidth]){
+  for(int i = 0; i< matrixHeight; i++) {
+    for(int j = 0; j< matrixWidth; j++) {
+      leds[i*matrixWidth + j] = matrix[loop_cnt%ESCENAS][i][j];
     }
   }
-  LEDS.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds,NUM_LEDS);
 }
 void setup() {
   Serial.begin(115200);
@@ -69,12 +68,12 @@ void setup() {
  
 
 void loop() {
-  changeLed()
+  
   unsigned long startMillis = millis();  // Start of sample window
-  unsigned int peakToPeak = 0;
+  unsigned short int peakToPeak = 0;
  
-  unsigned int signalMax = 0;
-  unsigned int signalMin = 1024;
+  unsigned short int signalMax = 0;
+  unsigned short int signalMin = 1024;
  
    while (millis() - startMillis < sampleWindow)
    {
@@ -93,9 +92,8 @@ void loop() {
    peakToPeak = signalMax - signalMin;
    int changeBrightness = map(peakToPeak, 20, 500, 0, 100);
    FastLED.setBrightness(changeBrightness);
-   stateHighLow(,matrixBajo,matrixAlto);
+   stateHighLow(peakToPeak<ruidoMedio,matrixBajo,matrixAlto);
    FastLED.show();
    Serial.println(peakToPeak);
-   loop_cnt++;
    FastLED.clear();
 }
